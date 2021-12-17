5291083b514a786c439b2e80d1323b3e9e76527a
jdhiren27 <joshidhiren07@gmail.com>
2021-12-17 22:18:12 +0530

    wishlist-image-resized

--- Frontend/src/Components/Dashboard/WishlistCardData.js
+++ Frontend/src/Components/Dashboard/WishlistCardData.js
@@ -5,7 +5,6 @@ import "animate.css";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
 
-
 const WishlistData = () => {
   const [gamedet, setGameDet] = useState([]);
   const [loaded, setLoaded] = useState(false);
@@ -21,7 +20,7 @@ const WishlistData = () => {
 
   const RemoveFromWishlist = (id) => {
     let newList = wishlist.filter((game) => game.gameID !== id);
-    setWishlist(newList)
+    setWishlist(newList);
     localStorage.setItem("Wishlist", JSON.stringify(newList));
     console.log("id", [wishlist.pop(id)]);
     console.log("newlist", wishlist);
@@ -51,11 +50,10 @@ const WishlistData = () => {
     };
   }, [DET_URL, wishlist]);
 
-
-console.log(gamedet, "gamedet");
+  console.log(gamedet, "gamedet");
   useEffect(() => {
-    let isCancelled = false
-    wishlist.length !==0 &&
+    let isCancelled = false;
+    wishlist.length !== 0 &&
       wishlist.map((game, index) => {
         return (
           <div key={index}>
@@ -64,9 +62,9 @@ console.log(gamedet, "gamedet");
                 `https://www.cheapshark.com/api/1.0/deals?storeID=1&steamAppID=${game.steamID}`
               )
               .then((res) => {
-                if (!isCancelled){
+                if (!isCancelled) {
                   setPrice((price) => price.concat(res.data));
-                  setLoaded(true)
+                  setLoaded(true);
                 }
               })
               .catch((err) => {
@@ -75,27 +73,28 @@ console.log(gamedet, "gamedet");
           </div>
         );
       });
-      return () => {
-        isCancelled = true
-      }
-
+    return () => {
+      isCancelled = true;
+    };
   }, [wishlist]);
   console.log(price, "price");
   let tempArr1 = [];
-console.log(tempArr1);
+  console.log(tempArr1);
 
-    return (
-      <>
+  return (
+    <>
       <div className="wishlistcard_header">
         <h4 className="own_h3">Games From Your Wishlist</h4>
         <NavLink to="/wishlist">
-        <h3 className="wishlist">see all</h3>
-      </NavLink>
+          <h3 className="wishlist">see all</h3>
+        </NavLink>
       </div>
       <div className="wishlistcard_wrapper">
         {loaded}
-        {wishlist.length !== 0 ?  (
-          gamedet.slice(0,3).map((game, index) => {
+        {wishlist.length !== 0 ? (
+          gamedet.slice(0, 3).map((game, index) => {
+            let gameurl = game.background_image.split("/");
+            let newURL = ` https://media.rawg.io/media/crop/600/400/${gameurl[4]}/${gameurl[5]}/${gameurl[6]}`;
             console.log("mad2", game.slug);
             return (
               <div id="wishlist_ctn" key={index}>
@@ -103,26 +102,29 @@ console.log(tempArr1);
                   key={index}
                   title={game.name}
                   gameID={game.id}
-                  image={game.background_image}
+                  image={newURL}
                   rem={() => RemoveFromWishlist(game.id)}
-                  slug = {game.slug}
+                  slug={game.slug}
                   plat={game.parent_platforms}
                 />
               </div>
             );
           })
-        ) 
-        : (
+        ) : (
           <div className="explore_main_container">
-          <div className="error-text">
-            <h3 className="error-head">You haven't added anything to your wishlist yet.</h3>
-            <p className="error-option"><NavLink to="/explore">Explore Now</NavLink></p>
+            <div className="error-text">
+              <h3 className="error-head">
+                You haven't added anything to your wishlist yet.
+              </h3>
+              <p className="error-option">
+                <NavLink to="/explore">Explore Now</NavLink>
+              </p>
+            </div>
           </div>
-      </div>
         )}
       </div>
-      </>
-    );
+    </>
+  );
 };
 
 export default WishlistData;
