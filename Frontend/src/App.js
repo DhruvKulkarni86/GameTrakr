35c0b7c788ee22ac2a12df9b3d80162934e4d8ed
DhruvKulkarni86 <dhruvkulkarni86@gmail.com>
2021-12-17 22:01:00 +0530

    updated app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,11 +4,17 @@ import './index.css';
 import {Route, Switch} from 'react-router-dom';
 import Search from './Components/Search/Search';
 import MainRes from './Components/MainRes/MainRes';
-// import Explore from './Components/Explore/Explore';
+import Explore from './Components/Explore/Explore';
 import GenreMain from './Components/Explore/GenreMain/GenreMain';
 import Reg from './Components/Auth/Register/Reg';
 import DevDisplay from './Components/DevDisplay/DevDisplay';
 import Wild from './Components/Wild/Wild';
+import EpicGames from "./Components/Explore/SearchByStores/EpicGames";
+import Steam from "./Components/Explore/SearchByStores/SteamGames";
+import Uplay from "./Components/Explore/SearchByStores/Uplay";
+import WishlistData from "./Components/Dashboard/WishlistData";
+import Dash from './Components/Dashboard/Dash'
+
 const App = () => {
   // let user = localStorage.getItem("Username");
   return (
@@ -18,9 +24,14 @@ const App = () => {
                   <Route path="/search" component={Search} />
                   <Route path="/view/:gameid/:slug" component={MainRes} />
                   <Route path="/genre/:genrename/:genreid" component={GenreMain} />
-                  {/* <Route path="/explore" component={Explore} /> */}
+                  <Route path="/explore" component={Explore} />
                   <Route path="/reg" component={Reg} />
                   <Route path="/dev/:devId/:devName" component={DevDisplay} />
+                  <Route path="/dashboard" component={Dash} />
+                  <Route path="/wishlist" component={WishlistData} />
+                  <Route path="/epicgames" component={EpicGames} />
+                  <Route path="/steam" component={Steam} />
+                  <Route path="/gog" component={Uplay} />
                   <Route path="*" component={Wild}/>
         </Switch>
     </div>
